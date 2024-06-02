ac8c8ba2ea5e0a6a57a3f3dcf4d9b7d9d641268f
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2024-06-02 19:12:39 -0400

    Add HTTPS server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require("dotenv-safe").config();
 const express = require("express");
 const http = require("http");
+const https = require("https");
 const helmet = require("helmet");
 
-const { HTTP_PORT } = process.env;
+const { HTTP_PORT, HTTPS_PORT, HTTPS } = process.env;
 
 const app = express();
 
 app.use(helmet());
 
-const httpServer = http.createServer(app);
-httpServer.listen(HTTP_PORT, () => {
-  console.log(`HTTP server started on port ${HTTP_PORT}`);
-});
+if (parseInt(HTTPS)) {
+  const options = {};
+  const httpsServer = https.createServer(options, app);
+  httpsServer.listen(HTTPS_PORT, () => {
+    console.log(`HTTPS server started on port ${HTTPS_PORT}`);
+  });
+} else {
+  const httpServer = http.createServer(app);
+  httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP server started on port ${HTTP_PORT}`);
+  });
+}
