a6c6611966fe37ee7b892232a94a977dfc5d39a0
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2024-06-02 20:02:56 -0400

    Add strict transport security and ssl configs

--- index.js
+++ index.js
@@ -4,14 +4,41 @@ const http = require("http");
 const https = require("https");
 const helmet = require("helmet");
 
-const { HTTP_PORT, HTTPS_PORT, HTTPS } = process.env;
+const {
+  HTTP_PORT,
+  HTTPS_PORT,
+  HTTPS,
+  HSTS_MAX_AGE,
+  HSTS_PRELOAD,
+  HSTS_INCLUDE_SUBDOMAINS,
+  SSL_MIN_VERSION,
+  SSL_MAX_VERSION,
+  SSL_CIPHERS,
+} = process.env;
 
 const app = express();
 
-app.use(helmet());
+app.use(
+  helmet({
+    strictTransportSecurity: parseInt(HTTPS)
+      ? {
+          maxAge: parseInt(HSTS_MAX_AGE),
+          includeSubDomains: !!parseInt(HSTS_INCLUDE_SUBDOMAINS),
+          preload:
+            parseInt(HSTS_PRELOAD) &&
+            parseInt(HSTS_INCLUDE_SUBDOMAINS) &&
+            parseInt(HSTS_MAX_AGE) >= 31536000,
+        }
+      : false,
+  })
+);
 
 if (parseInt(HTTPS)) {
-  const options = {};
+  const options = {
+    minVersion: SSL_MIN_VERSION,
+    maxVersion: SSL_MAX_VERSION,
+    ciphers: SSL_CIPHERS,
+  };
   const httpsServer = https.createServer(options, app);
   httpsServer.listen(HTTPS_PORT, () => {
     console.log(`HTTPS server started on port ${HTTPS_PORT}`);
